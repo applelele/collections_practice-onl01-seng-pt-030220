b039d2081af34a7074327e2c4d197399d33ce947
YUKIKO IKEDA <applelele@gmail.com>
2020-03-12 23:58:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,8 +18,6 @@ def swap_elements(array)
   end
 end
 
-def swap_elements_from_to(array, index, destination_index)
-  array.sort do |a, b|
-    a[index] <=> b[destination_index]
-  end
+def reverse_array(array)
+  
 end
